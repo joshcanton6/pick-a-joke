b9d0259f7e7d61fe52cae755acb5789fbfbacbe4
joshcanton6 <60484057+joshcanton6@users.noreply.github.com>
2024-05-03 08:46:47 -0700

    Added clean jokes

---
+++ clean/script.js
@@ -0,0 +1,16 @@
+async function fetchRandomJoke() {
+    var joke = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+        .then(response => response.json());
+
+    if (joke.type == "twopart") document.getElementById('joke').innerHTML = joke.setup + "<br>" + joke.delivery;
+    else document.getElementById('joke').innerHTML = joke.joke;
+}
+
+function setBackground() {
+    document.body.style.backgroundImage = "url(https://picsum.photos/" + window.innerWidth + "/" + window.innerHeight + ")";
+}
+
+function loadContent()  {
+    setBackground();
+    fetchRandomJoke();
+}
